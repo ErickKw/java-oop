8a247aeeadaec8bf2e6f1d9a60f5da603a772138
ErickKw <erickkusumaw@gmail.com>
2022-12-18 19:28:41 +0700

    deque arraydeque

--- src/JavaCollection/Deque/MainDeque.java
+++ src/JavaCollection/Deque/MainDeque.java
@@ -2,6 +2,7 @@ package JavaCollection.Deque;
 
 import JavaCollection.model.Person;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 
@@ -11,11 +12,18 @@ public class MainDeque {
         persons.offerLast(new Person(1, "Erick", "Trenggalek"));
         persons.offerFirst(new Person(2, "Kusuma", "Trenggalek"));
         persons.offerLast(new Person(3, "Wardani", "Trenggalek"));
-
-//        persons.offerLast("Kusuma");
-//        persons.offerLast("Wardani");
-
         for (var item = persons.pollLast(); item != null; item = persons.pollLast()){
+            System.out.println(item);
+        }
+
+
+        Deque <String> persons2 =  new ArrayDeque<>();
+        persons2.offerLast("Denjaka");
+        persons2.offerLast("Wardani");
+        persons2.offerFirst("Kusuma");
+
+
+        for (var item = persons2.pollLast(); item != null; item = persons2.pollLast()){
             System.out.println(item);
         }
     }
